ed1c6ff61d52e6addd5464db23e66e124c91d381
Ashish <deshpandeashish90@gmail.com>
2023-07-10 17:02:23 +0530

    modified system.sh

--- system.sh
+++ system.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+#This is demo script to display sysinfo 
+# Check Virtualization Extension
+grep -E -c '(vmx|svm)' /proc/cpuinfo
+echo "Run this command to make sure you’ve enabled virtualization in on your computer. It should be above 0"
+tree .
 echo "Date"
 date
 echo "Uptime"
@@ -9,3 +14,4 @@ echo "Network Usage"
 ip a
 echo "System Info"
 uname -a
+
