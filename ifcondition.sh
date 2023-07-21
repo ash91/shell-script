7631bd3dfae3f9c27be9f7127ed463a49abc0ce1
ashish <deshpandeashish90@gmail.com>
2023-07-21 13:35:09 +0530

    updated ifstatement

--- ifcondition.sh
+++ ifcondition.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
-salary=1000
-expenses=800
-#Check if salary and expenses are equal
-if [ $salary == $expenses ];
+
+if [ -f ~/Downloads/Joplin-2.11.11.AppImage ];
 then
-    echo "Salary and expenses are equal"
-#Check if salary and expenses are not equal
-elif [ $salary != $expenses ];
-then
-    echo "Salary and expenses are not equal"
+   echo "file exits"
+else
+   echo "file does not exits"
 fi
+
+pkg=htop
+
+if command -v $pkg
+then
+   echo  "htop is installed"
+else
+   echo "$pkg is not available in default path installing now"
+   sudo pacman -S $pkg --noconfirm
+fi
+$pkg
